4e3f54fda1724922b0123d367520ccefe2c66dcd
Scott Caratozzolo <scaratozzolo12@gmail.com>
2021-11-28 19:30:56 -0500

    create template for final

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from flask import jsonify, request, url_for
 from app import app
-import requests
 # This line import your functions from the services folder
 from app.services import *
 
@@ -8,19 +7,19 @@ from app.services import *
 @app.route("/")
 def index():
 
-    resp = """FA595 Midterm Fall 2021
+    resp = """FA595 Final Fall 2021
     <br /><br />
     Natalia Azarian<br />
     Scott Caratozzolo<br />
     Audrey Nguyen<br />
     Agathe Sadeghi
     <br /><br />
-    More information and documentation: <a href='https://github.com/scaratozzolo/FA595_Midterm'>https://github.com/scaratozzolo/FA595_Midterm</a>"""
+    More information and documentation: <a href='https://github.com/scaratozzolo/FA595_Final'>https://github.com/scaratozzolo/FA595_Final</a>"""
 
     return resp
 
 
-@app.route("/nlp", methods=["POST"])
+@app.route("/api", methods=["POST"])
 def nlp():
     # Whatever you send to the server will then print in the console
     print(request.json)
@@ -28,19 +27,19 @@ def nlp():
     # Whatever you send to the server will be returned back from the api
     return jsonify(request.json)
 
-@app.route("/nlp/services", methods=["GET", "POST"])
+@app.route("/api/services", methods=["GET", "POST"])
 def services():
 
     __services = {
                 "all": all_service,
-                "chat_bot": chat_bot_service,
-                "next_word": next_word_service,
-                "word_freq": word_frequency_service,
-                "word_lem": word_lemmatization_service,
-                "entity_ext": entity_ext_service,
-                "text_sentiment": text_sentiment_service,
-                "spellcheck": spellcheck_service,
-                "translate": translate_service
+                # "chat_bot": chat_bot_service,
+                # "next_word": next_word_service,
+                # "word_freq": word_frequency_service,
+                # "word_lem": word_lemmatization_service,
+                # "entity_ext": entity_ext_service,
+                # "text_sentiment": text_sentiment_service,
+                # "spellcheck": spellcheck_service,
+                # "translate": translate_service
         }
 
     if request.method == "GET":
@@ -63,7 +62,7 @@ def services():
 
         return jsonify(response)
 
-@app.route("/nlp/services/all", methods=["POST"])
+@app.route("/api/services/all", methods=["POST"])
 def all_service(data=None):
 
     if not data:
@@ -72,133 +71,28 @@ def all_service(data=None):
             return jsonify({"error":"no data provided"})
 
     services = {}
-    services["chat_bot"] = chat_bot_service(data).get_json()
-    services["next_word"] = next_word_service(data).get_json()
-    services["word_freq"] = word_frequency_service(data).get_json()
-    services["word_lem"] = word_lemmatization_service(data).get_json()
-    services["entity_ext"] = entity_ext_service(data).get_json()
-    services["text_sentiment"] = text_sentiment_service(data).get_json()
-    services["spellcheck"] = spellcheck_service(data).get_json()
-    services["translate"] = translate_service(data).get_json()
+    # services["chat_bot"] = chat_bot_service(data).get_json()
+    # services["next_word"] = next_word_service(data).get_json()
+    # services["word_freq"] = word_frequency_service(data).get_json()
+    # services["word_lem"] = word_lemmatization_service(data).get_json()
+    # services["entity_ext"] = entity_ext_service(data).get_json()
+    # services["text_sentiment"] = text_sentiment_service(data).get_json()
+    # services["spellcheck"] = spellcheck_service(data).get_json()
+    # services["translate"] = translate_service(data).get_json()
 
     return jsonify(services)
 
-@app.route("/nlp/services/chat_bot", methods=["POST"])
-def chat_bot_service(data=None):
+@app.route("/api/services/template", methods=["POST"])
+def template_service(data=None):
 
     if not data:
         data = request.json
         if not data:
             return jsonify({"error":"no data provided"})
-
-    chat_id = None
 
     if "text" not in data:
         return jsonify({"error":"'text' missing from payload"})
-    elif "chat_id" in data:
-        try:
-            chat_id = int(data['chat_id'])
-        except:
-            return jsonify({"error":f"invalid 'chat_id' in payload. given: {data['chat_id']}, type: {type(data['chat_id'])}"})
 
-    return jsonify(chat_bot(text=data['text'], chat_id=chat_id))
+    return jsonify(template(text=data['text']))
 
 
-@app.route("/nlp/services/next_word", methods=["POST"])
-def next_word_service(data=None):
-    
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error":"no data provided"})
-
-    k = 5
-    if "text" not in data:
-        return jsonify({"error":"'text' missing from payload"})
-    elif "num_words" in data:
-        try:
-            k = int(data['num_words'])
-        except:
-            return jsonify({"error":f"invalid 'num_words' in payload. given: {data['num_words']}, type: {type(data['num_words'])}"})
-
-    return jsonify(next_word(text=data['text'], k=k))
-
-
-@app.route("/nlp/services/word_freq", methods=["POST"])
-def word_frequency_service(data=None):
-    
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error":"no data provided"})
-
-    if "text" not in data:
-        return jsonify({"error": "'text' missing from payload"})
-
-    return jsonify(word_freq(text=data['text']))
-
-
-@app.route("/nlp/services/word_lem", methods=["POST"])
-def word_lemmatization_service(data=None):
-    
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error":"no data provided"})
-
-    if "text" not in data:
-        return jsonify({"error": "'text' missing from payload"})
-
-    return jsonify(word_lem(text=data['text']))
-
-@app.route("/nlp/services/entity_ext", methods=["POST"])
-def entity_ext_service(data=None):
-
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error": "no data provided"})
-
-    if "text" not in data:
-        return jsonify({"error": "'text' missing from payload"})
-
-    return jsonify(entity_ext(text=data['text']))
-
-
-@app.route("/nlp/services/text_sentiment", methods=["POST"])
-def text_sentiment_service(data=None):
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error": "no data provided"})
-
-    if "text" not in data:
-        return jsonify({"error": "'text' missing from payload"})
-
-    return jsonify(text_sentiment(text=data['text']))
-
-@app.route("/nlp/services/spellcheck", methods=["POST"])
-def spellcheck_service(data=None):
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error": "no data provided"})
-
-    if "text" not in data:
-        return jsonify({"error": "'text' missing from payload"})
-
-    return jsonify(spellcheck(text=data['text']))
-
-@app.route("/nlp/services/translate", methods=["POST"])
-def translate_service(data=None):
-    if not data:
-        data = request.json
-        if not data:
-            return jsonify({"error": "no data provided"})
-
-    if "text" not in data:
-        return jsonify({"error": "'text' missing from payload"})
-    elif "language_code" not in data:
-        return jsonify({"error":"'language_code' missing from payload"})
-
-    return jsonify(translate(text=data['text'], language=data['language_code']))
