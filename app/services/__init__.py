cebd8fc9063c71832bc913c225e503453abe91d6
Agathe Sadeghi <55772710+Agatheee@users.noreply.github.com>
2021-12-13 19:53:35 -0500

    Update __init__.py

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,6 +1,6 @@
 # add your functions here after they've been made
 # from .your_file_name import your_function1, your_function2
 from .scott_services import lstm_model, PortOpt
-# from .agathe_services import max_ret, min_risk
+from .agathe_services import max_ret, min_risk
 # from .audrey_services import entity_ext, text_sentiment
 # from .natalia_services import spellcheck, translate
