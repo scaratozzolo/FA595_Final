7e949b0936ede0f50b1ab695b0eef6a2b5f9e37a
Natalia Azarian <nataliaazarian@gmail.com>
2021-12-13 20:44:57 -0500

    remove error handling

--- app/services/natalia_services.py
+++ app/services/natalia_services.py
@@ -2,54 +2,42 @@ import yfinance as yf
 import numpy as np
 from sklearn.linear_model import LinearRegression
 
-def beta(ticker, start_dt, end_dt, inter):
+def beta(tickers, start_dt, end_dt, inter):
+
+    # Import daily adjusted close (stock price) data for specific tickers and date range / interval
+    data = yf.download(tickers, start=start_dt, end=end_dt, interval=inter)['Adj Close']
     
-    # Check that two tickers are inputted
-    if len(ticker) != 2:
-        print("Input two tickers.")
-    else:
-        # Import daily adjusted close (stock price) data for specific tickers and date range / interval
-        data = yf.download(ticker, start=start_dt, end=end_dt, interval=inter)['Adj Close']
-        
-        # Convert historical stock prices to daily percent change
-        price_change = data.pct_change()
-        
-        # Drop NaN
-        df = price_change.dropna()
-        
-        # Create arrays for x and y variables in the regression model
-        # Linear regression will only apply to first two tickers inputted
-        x = np.array(df.iloc[:, 0]).reshape((-1,1))
-        y = np.array(df.iloc[:, 1])
-        
-        # Define the model and type of regression
-        model = LinearRegression().fit(x, y)
+    # Convert historical stock prices to daily percent change
+    price_change = data.pct_change()
     
+    # Drop NaN
+    df = price_change.dropna()
+    
+    # Create arrays for x and y variables in the regression model
+    # Linear regression will only apply to first two tickers inputted
+    x = np.array(df.iloc[:, 0]).reshape((-1,1))
+    y = np.array(df.iloc[:, 1])
+    
+    # Define the model and type of regression
+    model = LinearRegression().fit(x, y)
+
     return {"Beta": float(model.coef_)}
 
-def sharpe(ticker, start_dt, end_dt, inter, weights):
-    
-    # Check that weight inputted for each symbol
-    if len(weights) != len(ticker):
-        print("Enter a weight for each ticker.")
-    else:
-        # Check that weights = 1
-        if sum(weights) != 1:
-            print("Check that weights total 100%.")
-        else:
-            # Import daily adjusted close (stock price) data for specific tickers and date range / interval
-            data = yf.download(ticker, start=start_dt, end=end_dt, interval=inter)['Adj Close']
+def sharpe(tickers, start_dt, end_dt, inter, weights):
 
-            # Calculate log return of the portfolio
-            log_return = np.sum(np.log(data/data.shift())*weights, axis=1)
-            
-            # Calculate the Sharpe Ratio
-            sharpe_ratio = log_return.mean()/log_return.std()
+    # Import daily adjusted close (stock price) data for specific tickers and date range / interval
+    data = yf.download(tickers, start=start_dt, end=end_dt, interval=inter)['Adj Close']
+
+    # Calculate log return of the portfolio
+    log_return = np.sum(np.log(data/data.shift())*weights, axis=1)
+    
+    # Calculate the Sharpe Ratio
+    sharpe_ratio = log_return.mean()/log_return.std()
 
     return {"Sharpe Ratio": sharpe_ratio}
  
 
-# ticker = ['SPY', 'AXS'] # input x-variable first
+# tickers = ['SPY', 'AXS'] # input x-variable first
 # start_dt = '2020-02-22' # input as YYYY-MM-DD
 # end_dt = '2020-03-22' # input as YYYY-MM-DD
 # inter = '1d' # specify the interval: 1m, 2m, 5m, 15m, 30m, 60m, 90m, 1d, 5d, 1wk, 1mo, 3mo
